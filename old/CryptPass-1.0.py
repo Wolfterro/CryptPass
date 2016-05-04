0a6d25726b93a2cfbd33e7c4b406548d3e8c4c0d
Wolfterro <wolfterrorista@hotmail.com>
2016-05-04 11:19:38 -0300

    Programa enviado ao Github!

--- old/CryptPass-1.0.py
+++ old/CryptPass-1.0.py
@@ -5,7 +5,6 @@ from __future__ import print_function
 import random
 import string
 import binascii
-import ConfigParser
 import getpass
 import time
 import os
@@ -13,13 +12,13 @@ import sys
 
 #--------------------------------------------
 # Criado por: Wolfterro
-# Versão: 1.1 - Python 2.x
-# Data: 04/05/2016
+# Versão: 1.0 - Python 2.x
+# Data: 01/05/2016
 #--------------------------------------------
 
 # Versão do Programa
 #===================
-version = "1.1"
+version = "1.0"
 
 # Verificando Existência da pasta "CryptPass"
 #============================================
@@ -37,25 +36,6 @@ def check_cryptpass_folder():
 	else:
 		os.chdir("CryptPass")
 
-# Verificando Private Key
-#========================
-def check_private_key():
-    config_check = ConfigParser.ConfigParser()
-    config_check.read("../Private.key")
-
-    try:
-        private_key = config_check.get("Private Key", "Key")
-        return private_key
-    except ConfigParser.Error:
-        print ("Private Key não existe! Criando...")
-        new_private_key = get_random_seed()
-        new_private_key_hex = string_to_hex(new_private_key)
-
-        file = open("../Private.key", "w")
-        file.write("[Private Key]" + "\n" + "Key: " + new_private_key_hex)
-        file.close()
-        return new_private_key_hex
-
 # Gerando Timestamp Para o Arquivo
 #=================================
 def get_timestamp_for_file():
@@ -67,51 +47,40 @@ def get_timestamp_for_file():
 def get_random_seed():
 	return "".join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(64))
 
-# Método de String para Hexadecimal
-#==================================
-def string_to_hex(string):
-    result_hex = hex(int(binascii.hexlify(string), 16))
-    return result_hex.replace("0x", "").replace("L", "").upper()
-
-# Método de Hexadecimal para String
-#==================================
-def hex_to_string(hex_dump):
-    hex_dump_int = int(hex_dump, 16)
-    hex_dump_string = binascii.unhexlify("%x" % hex_dump_int)
-    return hex_dump_string
+# Método de String para Binário
+#==============================
+def string_to_binary(string):
+	result_bin = bin(int(binascii.hexlify(string), 16))
+	return result_bin
 
 # Método Principal: Encriptando Senha
 #====================================
 def main_encrypt_password():
-	# Resgatando Private Key
-	#=======================
-	get_private_key = check_private_key()
-
 	# Resgatando Senha
 	#=================
 	print("")
 	get_password = getpass.getpass("Insira sua senha: ")
-	get_password_hex = string_to_hex(get_password)
+	get_password_bin = string_to_binary(get_password)
 
 	# Gerando Seed
 	#=============
-	get_seed = get_random_seed()
-	get_seed_hex = string_to_hex(get_seed)
+	generated_seed = get_random_seed()
+	generated_seed_bin = string_to_binary(generated_seed)
 
 	# Gerando Master Key
 	#===================
-	master_key_hex = hex(int(get_private_key, 16) * (int(get_password_hex, 16) + int(get_seed_hex, 16))).replace("0x", "").replace("L", "").upper()
+	master_key_bin = bin(int(generated_seed_bin, 2) + int(get_password_bin, 2))
 
 	# Enviando Informações Para o Usuário
 	#====================================
-	print ("\nMaster Key: %s\n" % (master_key_hex))
-	print ("Seed: %s\n" % (get_seed_hex))
+	print ("\nMaster Key: %s\n" % (master_key_bin))
+	print ("Seed: %s\n" % (generated_seed_bin))
 
 	# Salvando em Arquivo
 	#====================
 	file_generated = "CryptPass_-_" + get_timestamp_for_file() + ".txt"
 	file = open(file_generated, "w")
-	file.write("Master Key: " + master_key_hex + "\n" + "Seed: " + get_seed_hex + "\n")
+	file.write("Master Key: " + master_key_bin + "\n" + "Seed: " + generated_seed_bin + "\n")
 	file.close()
 
 	print("Arquivo criado em: %s" % (os.path.realpath(file_generated)))
@@ -119,12 +88,6 @@ def main_encrypt_password():
 # Método Principal: Decriptando Senha
 #====================================
 def main_decrypt_password():
-	# Resgatando Private Key
-	#=======================
-	get_private_key = check_private_key()
-
-	# Resgatando Arquivo
-	#===================
 	escolher_arquivo = raw_input("\nEscolha um arquivo através do número listado: ")
 	escolher_arquivo = int(escolher_arquivo)
 
@@ -141,11 +104,12 @@ def main_decrypt_password():
 		get_master_key_from_file = get_master_key_from_file.replace("Master Key: ", "")
 		get_seed_from_file = get_seed_from_file.replace("Seed: ", "")
 
-		master_key_decrypted = hex((int(get_master_key_from_file, 16) / int(get_private_key, 16)) - int(get_seed_from_file, 16)).replace("0x", "").replace("L", "").upper()
-		password_string = hex_to_string(master_key_decrypted)
+		master_key_decrypted = bin(int(get_master_key_from_file, 2) - int(get_seed_from_file, 2))
+		master_key_decrypted_int = int(master_key_decrypted, 2)
+		password_plain_text = binascii.unhexlify("%x" % master_key_decrypted_int)
 
 		print ("Master Key Decriptada: %s" % (master_key_decrypted))
-		print ("\nSenha: %s" % (password_string))
+		print ("\nSenha: %s" % (password_plain_text))
 
 		file.close()
 
