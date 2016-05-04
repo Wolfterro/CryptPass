47534d910403504681355326dc47478308187e4c
Wolfterro <wolfterrorista@hotmail.com>
2016-05-04 14:17:54 -0300

    Programa enviado ao Github!

--- CryptPass.py
+++ CryptPass.py
@@ -18,7 +18,7 @@ import sys
 #--------------------------------------------
 # Copyright (c) 2016 Wolfgang Almeida <wolfgang.almeida@yahoo.com>
 #-----------------------------------------------------------------------
-#	This program is free software: you can redistribute it and/or modify
+#   This program is free software: you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
 #   the Free Software Foundation, either version 3 of the License, or
 #   (at your option) any later version.
